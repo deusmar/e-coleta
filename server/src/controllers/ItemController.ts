9992ac3613da07b6743eda4f377049e9c449d08e
Deusmar Júnior <deusmar.junior@outlook.com>
2020-06-23 18:21:31 -0300

    [NLW#04] React native app

--- server/src/controllers/ItemController.ts
+++ server/src/controllers/ItemController.ts
@@ -9,7 +9,7 @@ class ItemController {
         return {
             id: item.id,
             name: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`
+            image_url: `http://192.168.101.15:3333/uploads/${item.image}`
         }
     })
 
